8775dfbf21358e15f435d933c4a2381d94ccf713
cssxsh <cssxsh@gmail.com>
2024-03-14 02:33:36 +0800

    Q.0412

---
+++ src/main/kotlin/xyz/cssxsh/leetcode/editor/cn/No0412FizzBuzz.kt
@@ -0,0 +1,35 @@
+package xyz.cssxsh.leetcode.editor.cn
+
+class No0412FizzBuzz {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        fun fizzBuzz(n: Int): List<String> {
+            var fb = 15
+            var f = 3
+            var b = 5
+            return MutableList(size = n) { index ->
+                when (index + 1) {
+                    fb -> {
+                        fb += 15
+                        f += 3
+                        b += 5
+                        "FizzBuzz"
+                    }
+                    f -> {
+                        f += 3
+                        "Fizz"
+                    }
+                    b -> {
+                        b += 5
+                        "Buzz"
+                    }
+                    else -> {
+                        (index + 1).toString()
+                    }
+                }
+            }
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
