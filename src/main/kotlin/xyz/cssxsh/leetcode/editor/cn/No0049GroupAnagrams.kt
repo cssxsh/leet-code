3f37991d1d748ba119d142589e8f3c66ea758370
cssxsh <cssxsh@gmail.com>
2024-03-14 06:54:13 +0800

    Q.0049

---
+++ src/main/kotlin/xyz/cssxsh/leetcode/editor/cn/No0049GroupAnagrams.kt
@@ -0,0 +1,15 @@
+package xyz.cssxsh.leetcode.editor.cn
+
+object No0049GroupAnagrams {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        fun groupAnagrams(strs: Array<String>): List<List<String>> {
+            return strs.groupBy { str -> str.sort() }.values.toList()
+        }
+
+        private fun String.sort(): String {
+            return String(toCharArray().apply { sort() })
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+}
