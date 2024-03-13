ff0b90970ae576ebaefad3dd1b07de23e53acda1
cssxsh <cssxsh@gmail.com>
2024-03-14 02:18:09 +0800

    Q.1672

---
+++ src/main/kotlin/xyz/cssxsh/leetcode/editor/cn/No1672RichestCustomerWealth.kt
@@ -0,0 +1,16 @@
+package xyz.cssxsh.leetcode.editor.cn
+
+class No1672RichestCustomerWealth {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        fun maximumWealth(accounts: Array<IntArray>): Int {
+            val sums = IntArray(size = accounts.size) { 0 }
+            accounts.forEachIndexed { i, account ->
+                sums[i] = account.sum()
+            }
+            return sums.max()
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
