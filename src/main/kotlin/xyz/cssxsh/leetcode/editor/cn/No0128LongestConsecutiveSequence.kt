4e70b0cddf0508e1f5684fbbfcb812eb5551445c
cssxsh <cssxsh@gmail.com>
2024-03-14 07:39:13 +0800

    Q.0128

---
+++ src/main/kotlin/xyz/cssxsh/leetcode/editor/cn/No0128LongestConsecutiveSequence.kt
@@ -0,0 +1,21 @@
+package xyz.cssxsh.leetcode.editor.cn
+
+object No0128LongestConsecutiveSequence {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        fun longestConsecutive(nums: IntArray): Int {
+            val map = hashMapOf<Int, Int>()
+            for (num in nums) {
+                if (num in map) continue
+                val l = map.getOrDefault(key = num - 1, defaultValue = 0)
+                val r = map.getOrDefault(key = num + 1, defaultValue = 0)
+                val sum = l + 1 + r
+                map[num] = sum
+                map[num - l] = sum
+                map[num + r] = sum
+            }
+            return map.values.maxOrNull() ?: 0
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+}
