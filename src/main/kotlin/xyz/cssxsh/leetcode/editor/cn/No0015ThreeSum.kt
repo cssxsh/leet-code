3ad39f406b9172cb976323f6b4dd315cc5c30b66
cssxsh <cssxsh@gmail.com>
2024-03-14 14:19:30 +0800

    Q.0015

---
+++ src/main/kotlin/xyz/cssxsh/leetcode/editor/cn/No0015ThreeSum.kt
@@ -0,0 +1,43 @@
+package xyz.cssxsh.leetcode.editor.cn
+
+object No0015ThreeSum {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        fun threeSum(nums: IntArray): List<List<Int>> {
+            nums.sort()
+            val list = mutableListOf<List<Int>>()
+
+            var i = 0
+            var p = null as Int?
+            while (i < nums.size - 2) {
+                if (nums[i] == p) {
+                    i++
+                    continue
+                }
+
+                var j = i + 1
+                var k = nums.size - 1
+                var q = null as Int?
+                while (j < k) {
+                    if (nums[k] == q) {
+                        k--
+                        continue
+                    }
+
+                    val sum = nums[i] + nums[j] + nums[k]
+                    if (sum == 0) {
+                        list.add(element = listOf(nums[i], nums[j], nums[k]))
+                        p = nums[i]
+                        q = nums[k]
+                    }
+                    if (sum >= 0) k--
+                    if (sum <= 0) j++
+                }
+
+                i++
+            }
+            return list
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+}
