93883cc75938a2aa27800d2eb50263fc04b33613
cssxsh <cssxsh@gmail.com>
2024-03-16 15:57:13 +0800

    style

--- src/test/kotlin/xyz/cssxsh/leetcode/editor/cn/No0169MajorityElementTest.kt
+++ src/test/kotlin/xyz/cssxsh/leetcode/editor/cn/No0169MajorityElementTest.kt
@@ -10,11 +10,11 @@ internal class No0169MajorityElementTest : AbstractSolutionTest() {
     override fun example() {
         Assertions.assertEquals(
             3,
-            solution.majorityElement(nums = intArrayOf(3,2,3))
+            solution.majorityElement(nums = intArrayOf(3, 2, 3))
         )
         Assertions.assertEquals(
             2,
-            solution.majorityElement(nums = intArrayOf(2,2,1,1,1,2,2))
+            solution.majorityElement(nums = intArrayOf(2, 2, 1, 1, 1, 2, 2))
         )
     }
 }
