357c127819614c871c5a6e601d6069bc2d384b40
cssxsh <cssxsh@gmail.com>
2024-03-18 06:44:30 +0800

    feat: parts

--- src/main/kotlin/xyz/cssxsh/leetcode/editor/cn/Reflect.kt
+++ src/main/kotlin/xyz/cssxsh/leetcode/editor/cn/Reflect.kt
@@ -4,38 +4,63 @@ import xyz.cssxsh.leetcode.editor.cn.data.*
 import kotlin.reflect.*
 import kotlin.reflect.full.*
 
+@PublishedApi
+internal fun String.parts(vararg delimiters: Char = charArrayOf(',')): Sequence<String> {
+    return sequence {
+        var start = 1
+        var index = 0
+        var text = false
+        var part = -1
+        while (index < length) {
+            when (get(index = index++)) {
+                '"' -> {
+                    text = !text
+                }
+                '[' -> {
+                    if (text) continue
+                    part++
+                }
+                ']' -> {
+                    if (text) continue
+                    part--
+                }
+                in delimiters -> {
+                    if (text) continue
+                    if (part != 0) continue
+                    yield(value = substring(startIndex = start, endIndex = index - 1))
+                    start = index
+                }
+            }
+        }
+        if (start < index - 1) yield(value = substring(startIndex = start, endIndex = index - 1))
+    }
+}
+
 private fun KParameter.parse(line: String): Any? {
     return when (type) {
         typeOf<String>() -> line.trim('"')
         typeOf<Int>() -> line.toInt()
         typeOf<Long>() -> line.toLong()
-        typeOf<IntArray>() -> line.trim('[', ']')
-            .splitToSequence(',')
+        typeOf<IntArray>() -> line.parts()
             .map { it.toInt() }
             .toList()
             .toIntArray()
-        typeOf<LongArray>() -> line.trim('[', ']')
-            .splitToSequence(',')
+        typeOf<LongArray>() -> line.parts()
             .map { it.toLong() }
             .toList()
             .toLongArray()
-        typeOf<ListNode>(), typeOf<ListNode?>() -> line.trim('[', ']')
-            .splitToSequence(',')
-            .filter { it.isNotEmpty() }
+        typeOf<ListNode>(), typeOf<ListNode?>() -> line.parts()
             .map { it.toInt() }
             .toListNode()
-        typeOf<TreeNode>(), typeOf<TreeNode?>() -> line.trim('[', ']')
-            .splitToSequence(',')
+        typeOf<TreeNode>(), typeOf<TreeNode?>() -> line.parts()
             .map { it.toIntOrNull() }
             .toTreeNode()
-        typeOf<Array<String>>() -> line.trim('[', ']')
-            .splitToSequence(',')
+        typeOf<Array<String>>() -> line.parts()
             .map { it.trim('"') }
             .toList()
             .toTypedArray()
-        typeOf<Array<IntArray>>() -> line.trim('[', ']')
-            .splitToSequence("],[")
-            .map { it.splitToSequence(',').map(String::toInt).toList().toIntArray() }
+        typeOf<Array<IntArray>>() -> line.parts()
+            .map { it.parts().map(String::toInt).toList().toIntArray() }
             .toList()
             .toTypedArray()
         else -> throw IllegalArgumentException("unsupported $this")
@@ -58,8 +83,7 @@ internal fun runSolution(classifier: KClass<*>, vararg args: String) {
     println(classifier.qualifiedName)
 
     val method = classifier.members.filter { it.visibility == KVisibility.PUBLIC }
-        .maxByOrNull { it.parameters.size }
-        ?: throw IllegalArgumentException("not found method")
+        .maxByOrNull { it.parameters.size } ?: throw IllegalArgumentException("not found method")
     val lines = args.ifEmpty { readExample(classifier = classifier) }
 
     for (offset in lines.indices step method.parameters.size - 1) {
@@ -107,32 +131,33 @@ internal inline fun <reified T : Any> runInstance(vararg args: String) {
 internal fun runInstance(classifier: KClass<*>, vararg args: String) {
     println(classifier.qualifiedName)
 
-    lateinit var instance: Any
-
+    var instance: Any? = null
 
     val lines = args.ifEmpty { readExample(classifier = classifier) }
 
-    val commands = lines[0].trim('[', ']')
-        .splitToSequence(',')
+    val commands = lines[0].parts()
         .map { it.trim('"') }
 
-    val parameters = lines[1].trim('[', ']')
-        .splitToSequence(',')
-        .map { it.trim('[', ']').splitToSequence(',').toList() }
+    val parameters = lines[1].parts()
+        .map { it.parts() }
 
-    for ((command, array) in commands zip parameters) {
-        if (command == classifier.simpleName) {
-            instance = classifier.createInstance()
-            continue
-        }
+    for ((command, parts) in commands zip parameters) {
         val method = classifier.members.find { it.name == command }
+            ?: classifier.constructors.find { classifier.simpleName == command }
             ?: throw IllegalArgumentException("not found method $command")
+        val iterator = parts.iterator()
 
         val values = method.parameters.associateWith { parameter ->
             if (parameter.kind == KParameter.Kind.INSTANCE) return@associateWith instance
-            parameter.parse(line = array[parameter.index - 1])
+            parameter.parse(line = iterator.next())
         }
         val result = method.callBy(args = values)
+
+        if (command == classifier.simpleName) {
+            instance = result!!
+            continue
+        }
+
         println("---")
         println("command: $command")
         when (result) {
